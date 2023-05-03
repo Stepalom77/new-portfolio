03b1453f3f0af19935b744b7706229eab4f97023
Stepalom77 <stepalom22@gmail.com>
2023-05-02 20:15:21 -0500

    taking out the contact info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './home/about-me/about-me.component';
-import { ContactMeComponent } from './home/contact-me/contact-me.component';
+//import { ContactMeComponent } from './home/contact-me/contact-me.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './home/projects/projects.component';
 
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent, children: [
     {path: 'about-me', component: AboutMeComponent},
     {path: 'projects', component: ProjectsComponent},
-    {path: 'contact-me', component: ContactMeComponent}
+    //{path: 'contact-me', component: ContactMeComponent}
   ]}
 ];
 
